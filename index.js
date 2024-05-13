a1a2603db1cf71f23788d2222005b6b99862a9be
Сергей Пимонов <doniguan001@gmail.com>
2024-05-13 19:57:21 +0300

    started to figure out logic a little bit more

--- index.js
+++ index.js
@@ -1,9 +1,39 @@
-function onload() {}
+let cells = []
+let timerToFall // таймер, за сколько фигура сдвигается вниз, при увеличении уровня уменьшается
+let figure
 
-function randomFigure() {}
+//как сделать вывод фигуры - через класс? координаты, цвет, частота?
 
-function randomFieldFullfilling() {}
+onload = function() {
+    cells = document.getElementsByClassName('cell')
+    figure = randomFigure()
+    figure.addEventListener('')
 
-function lineDisappearing() {}
+}
 
-function figureMove() {}
+function randomFigure() {
+    let random = Math.round(Math.random() * 5)
+    switch (random) {
+    case 1: return 1
+    case 2: return 2
+    case 3: return 3
+    case 4: return 4
+    case 5: return 5 }
+//в зависимости от рандома выбирается фигура и передаётся в figure. каждый раз новая?
+}
+
+function randomFieldFullfilling() {
+    //?
+}
+
+function lineDisappearing() {
+    //проверить в массиве построчно, всё ли заполнено, если заполнено, линия исчезает, верхние сдвигаются вниз, счетчик сколько линий исчезло, чем больше, тем больше очков
+}
+
+function figureMove() {
+    //в figure добавляется eventlistener, если в бок, то проверяется, не упирается ли в стенку, если вниз, то пропускается таймер и фигура резко свдигается на линию вниз, как лучше сделать?
+}
+
+function levelUp() {
+    //выводится сообщение, игра на пару секунд становится на паузу, уменьшается таймер
+}
